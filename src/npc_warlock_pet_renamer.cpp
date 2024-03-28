518571e594d5d3fa401c4afb01bf969ab8010077
silviu20092 <dobrescu.silviu92@gmail.com>
2024-03-28 18:35:53 +0200

    Silence warning

--- src/npc_warlock_pet_renamer.cpp
+++ src/npc_warlock_pet_renamer.cpp
@@ -125,7 +125,7 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action) override
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
     {
         if (action == GOSSIP_ACTION_INFO_DEF)
         {
